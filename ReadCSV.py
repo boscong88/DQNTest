7e228cf1f84274bffd55005f593386b0962c9714
boscong88 <boscong88@gmail.com>
2016-04-09 21:50:18 +0800

    Delete ReadCSV.py

--- ReadCSV.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open("C:\Projects\Yahoo.csv") as DataSourceFile:
-    reader = csv.DictReader(DataSourceFile) # assume first row is header row that gives fieldname
-    for row in reader:
-        print(row['Open'], row['Close'])
-
-        
